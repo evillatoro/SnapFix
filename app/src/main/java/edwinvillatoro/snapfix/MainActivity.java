596fd540930572b7b0713e1d49d29d9c53c064d1
evillatoro1 <villatoroedwin1@gmail.com>
2017-08-25 19:26:53 -0400

    camera takes and displays picture

--- app/src/main/java/edwinvillatoro/snapfix/MainActivity.java
+++ app/src/main/java/edwinvillatoro/snapfix/MainActivity.java
@@ -1,13 +1,56 @@
 package edwinvillatoro.snapfix;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageView mImageView;
+    private Button mCameraButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(mToolbar);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+
+        mCameraButton = (Button) findViewById(R.id.cameraButton);
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
---
+++ app/src/main/java/edwinvillatoro/snapfix/objects/User.java
@@ -0,0 +1,8 @@
+package edwinvillatoro.snapfix.objects;
+
+/**
+ * Created by edwinvillatoro on 8/25/17.
+ */
+
+public class User {
+}
