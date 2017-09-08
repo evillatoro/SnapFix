e69319a93acc24a54671f460fcd4bf1be757100c
Foo, Ollie <ofoo3@gatech.edu>
2017-09-08 11:35:26 -0400

    made reports class. made submit button in NoPictureActivity upload report to database

--- app/src/main/java/edwinvillatoro/snapfix/NoPictureActivity.java
+++ app/src/main/java/edwinvillatoro/snapfix/NoPictureActivity.java
@@ -1,23 +1,37 @@
 package edwinvillatoro.snapfix;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edwinvillatoro.snapfix.R;
+import edwinvillatoro.snapfix.objects.Report;
 
 public class NoPictureActivity extends AppCompatActivity {
 
     private Spinner typeSpinner;
     private TextView locationView;
+    private EditText descriptionBox;
     private Button submitButton;
     private Button cancelButton;
     private Toolbar noPicToolbar;
+
+    private FirebaseDatabase database;
+    private DatabaseReference reports_ref;
 
 
     @Override
@@ -27,11 +41,18 @@ public class NoPictureActivity extends AppCompatActivity {
 
         typeSpinner = (Spinner) findViewById(R.id.noPic_problemTypeBox);
         locationView = (TextView) findViewById(R.id.noPic_locationBox);
+        descriptionBox = (EditText) findViewById(R.id.noPic_descriptionBox);
         submitButton = (Button) findViewById(R.id.noPic_submitButton);
         cancelButton = (Button) findViewById(R.id.noPic_cancelButton);
         noPicToolbar = (Toolbar) findViewById(R.id.toolBar);
 
         setSupportActionBar(noPicToolbar);
+
+
+        database = FirebaseDatabase.getInstance();
+        reports_ref = database.getReference("reports");
+
+
 
         // fills spinner with values
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -39,6 +60,12 @@ public class NoPictureActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         typeSpinner.setAdapter(adapter);
 
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addReport();
+            }
+        });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +73,27 @@ public class NoPictureActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+
+
+    private void addReport() {
+        String type = typeSpinner.getSelectedItem().toString();
+        String description = descriptionBox.getText().toString();
+
+        if(!TextUtils.isEmpty(description)) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
+            String timestamp = simpleDateFormat.format(new Date());
+
+            String id = reports_ref.push().getKey();
+            Report report = new Report(id, timestamp, type, "N/A", description);
+            locationView.setText(id);
+            reports_ref.child(id).setValue(report);
+            Toast.makeText(this, "Your request has been submitted", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(this, "The description cannot be left blank", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/edwinvillatoro/snapfix/objects/Report.java
+++ app/src/main/java/edwinvillatoro/snapfix/objects/Report.java
@@ -1,8 +1,39 @@
 package edwinvillatoro.snapfix.objects;
 
-/**
- * Created by edwinvillatoro on 8/25/17.
- */
 
 public class Report {
+
+    private String id;
+    private String timestamp;
+    private String problem_type;
+    private String location;
+    private String description;
+
+    public Report(String id, String timestamp, String problem_type, String location, String description) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.problem_type = problem_type;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getProblem_type() {
+        return problem_type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
