08c93b1afd8571782325eb9255c9e91690a37cd9
evillatoro1 <villatoroedwin1@gmail.com>
2017-09-09 00:29:11 -0400

    Closes #7 Closes #6 Picture and Report Data shows up in Firebase

--- app/src/main/java/edwinvillatoro/snapfix/MainActivity.java
+++ app/src/main/java/edwinvillatoro/snapfix/MainActivity.java
@@ -1,11 +1,8 @@
 package edwinvillatoro.snapfix;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -19,64 +16,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
 
     private Toolbar mToolbar;
-    private ImageView mImage;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int PICK_IMAGE_REQUEST = 2;
     private Uri filePath;
-    private FirebaseStorage mFirebaseStorage;
-    private StorageReference mStorageReference;
-    private ProgressDialog mProgressDialog;
-    private Bitmap mImageBitmap;
 
-    private Button mBtnChooseFromGallery, mBtnCamera, mBtnUpload, mBtnNoPicture;
-
-
-    private static final int REQUEST_LOCATION = 1;
-    Button button;
-    TextView textView;
-    LocationManager locationManager;
-    String lattitude,longitude;
+    private Button mBtnChooseFromGallery, mBtnCamera, mBtnNoPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImage = (ImageView) findViewById(R.id.image);
         mBtnCamera = (Button) findViewById(R.id.btnCamera);
         mBtnChooseFromGallery = (Button) findViewById(R.id.btnChoose);
-        mBtnUpload = (Button) findViewById(R.id.btnUpload);
         mBtnNoPicture = (Button) findViewById(R.id.btnNoPicture);
-
-        mProgressDialog = new ProgressDialog(this);
-
-        // initialize Firebase
-        mFirebaseStorage = FirebaseStorage.getInstance();
-        mStorageReference = mFirebaseStorage.getReference();
 
         // set custom toolbar
         mToolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(mToolbar);
-
-        mImageBitmap = null;
 
         mBtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,13 +51,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onClick(View view) {
                 launchGalleryIntent();
-            }
-        });
-
-        mBtnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                uploadPictureToDatabase();
             }
         });
 
@@ -116,7 +71,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //TODO: make mBtnNoPicture take you to another activity
         mBtnNoPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -125,28 +79,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-
         //TODO: check permissions for going into gallery
-
         checkPermissions();
 
-        button = (Button) findViewById(R.id.button_location);
         ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                GpsTracker gt = new GpsTracker(getApplicationContext());
-                Location l = gt.getLocation();
-                if( l == null){
-                    Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
-                }else {
-                    double lat = l.getLatitude();
-                    double lon = l.getLongitude();
-                    Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 
     private void launchGalleryIntent() {
@@ -163,47 +100,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    private void uploadPictureToDatabase() {
-        // set the progress dialog
-        mProgressDialog.setMessage("Uploading Image...");
-        mProgressDialog.show();
-
-        if (mImageBitmap != null) {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            mImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-            byte[] dataBAOS = baos.toByteArray();
-
-            /*************** UPLOADS THE PIC TO FIREBASE***************/
-            StorageReference storageRef = mStorageReference.child("images");
-
-            //name of the image file (added time to have different files to avoid rewrite on the same file)
-            StorageReference imagesRef = storageRef.child("filename" + new Date().getTime());
-
-            //upload image
-            UploadTask uploadTask = imagesRef.putBytes(dataBAOS);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    // handle failed upload
-                    Toast.makeText(getApplicationContext(), "Uploading failed", Toast.LENGTH_SHORT).show();
-                    mProgressDialog.dismiss();
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    // handle successful upload
-                    Toast.makeText(getApplicationContext(), "Uploading successful", Toast.LENGTH_SHORT).show();
-                    mProgressDialog.dismiss();
-                }
-            });
-
-            mImageBitmap = null;
-        } else {
-            Toast.makeText(getApplicationContext(), "Null", Toast.LENGTH_SHORT).show();
-            mProgressDialog.dismiss();
-        }
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -211,20 +107,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             // get the camera image
             Bundle extras = data.getExtras();
-            mImageBitmap = (Bitmap) extras.get("data");
 
-            // set the image into the imageview
-            mImage.setImageBitmap(mImageBitmap);
+            Intent s = new Intent(MainActivity.this, NoPictureActivity.class);
+            s.putExtra("camera", true);
+            s.putExtra("imageBitmap", (Bitmap) extras.get("data"));
+            startActivity(s);
 
         } else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             filePath = data.getData();
 
-            try {
-                mImageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                mImage.setImageBitmap(mImageBitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                Intent s = new Intent(MainActivity.this, NoPictureActivity.class);
+                s.putExtra("gallery", true);
+                s.putExtra("filePath", data.getData());
+                startActivity(s);
         }
     }
 
--- app/src/main/java/edwinvillatoro/snapfix/NoPictureActivity.java
+++ app/src/main/java/edwinvillatoro/snapfix/NoPictureActivity.java
@@ -1,24 +1,37 @@
 package edwinvillatoro.snapfix;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import edwinvillatoro.snapfix.R;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import edwinvillatoro.snapfix.objects.Report;
 
 public class NoPictureActivity extends AppCompatActivity {
@@ -33,6 +46,14 @@ public class NoPictureActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference reports_ref;
 
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mStorageReference;
+
+    private Double latitude, longitude;
+    private ImageView imageView;
+    private ProgressDialog mProgressDialog;
+
+    private Bitmap mImageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,14 +66,37 @@ public class NoPictureActivity extends AppCompatActivity {
         submitButton = (Button) findViewById(R.id.noPic_submitButton);
         cancelButton = (Button) findViewById(R.id.noPic_cancelButton);
         noPicToolbar = (Toolbar) findViewById(R.id.toolBar);
+        imageView = (ImageView) findViewById(R.id.imageInActivity);
 
         setSupportActionBar(noPicToolbar);
 
+        mProgressDialog = new ProgressDialog(this);
+
+        // initialize Firebase
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mStorageReference = mFirebaseStorage.getReference();
 
         database = FirebaseDatabase.getInstance();
         reports_ref = database.getReference("reports");
 
-
+        mImageBitmap = null;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle!= null) {
+            if (bundle.getBoolean("camera")) {
+                mImageBitmap = getIntent().getExtras().getParcelable("imageBitmap");
+                imageView.setImageBitmap(mImageBitmap);
+            } else if (bundle.getBoolean("gallery")) {
+                Uri filePath = bundle.getParcelable("filePath");
+                try {
+                    mImageBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                    imageView.setImageBitmap(mImageBitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
 
         // fills spinner with values
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -73,15 +117,15 @@ public class NoPictureActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        getUserCurrentLocation();
     }
-
-
 
     private void addReport() {
         String type = typeSpinner.getSelectedItem().toString();
         String description = descriptionBox.getText().toString();
 
-        if(!TextUtils.isEmpty(description)) {
+        if (!TextUtils.isEmpty(description)) {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
             String timestamp = simpleDateFormat.format(new Date());
 
@@ -90,10 +134,66 @@ public class NoPictureActivity extends AppCompatActivity {
             locationView.setText(id);
             reports_ref.child(id).setValue(report);
             Toast.makeText(this, "Your request has been submitted", Toast.LENGTH_SHORT).show();
-            finish();
+            uploadPictureToDatabase();
         } else {
             Toast.makeText(this, "The description cannot be left blank", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private void getUserCurrentLocation() {
+        GpsTracker gt = new GpsTracker(getApplicationContext());
+        Location l = gt.getLocation();
+        if (l == null){
+            Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
+        } else {
+            double lat = l.getLatitude();
+            double lon = l.getLongitude();
+
+            latitude = lat;
+            longitude = lon;
+            Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadPictureToDatabase() {
+        // set the progress dialog
+        mProgressDialog.setMessage("Uploading Image...");
+        mProgressDialog.show();
+
+        if (mImageBitmap != null) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            mImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] dataBAOS = baos.toByteArray();
+
+            /*************** UPLOADS THE PIC TO FIREBASE***************/
+            StorageReference storageRef = mStorageReference.child("images");
+
+            //name of the image file (added time to have different files to avoid rewrite on the same file)
+            StorageReference imagesRef = storageRef.child("filename" + new Date().getTime());
+
+            //upload image
+            UploadTask uploadTask = imagesRef.putBytes(dataBAOS);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // handle failed upload
+                    Toast.makeText(getApplicationContext(), "Uploading failed", Toast.LENGTH_SHORT).show();
+                    mProgressDialog.dismiss();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // handle successful upload
+                    Toast.makeText(getApplicationContext(), "Uploading successful", Toast.LENGTH_SHORT).show();
+                    mProgressDialog.dismiss();
+                    finish();
+                }
+            });
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Null", Toast.LENGTH_SHORT).show();
+            mProgressDialog.dismiss();
+        }
     }
 }
