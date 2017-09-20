1bb1c1ac83acb6d252d873fe837d6a33dacbe5ea
Foo, Ollie <ofoo3@gatech.edu>
2017-09-20 12:28:49 -0400

    fixed bug that closes activity when register button is clicked

--- app/src/main/java/edwinvillatoro/snapfix/SignupActivity.java
+++ app/src/main/java/edwinvillatoro/snapfix/SignupActivity.java
@@ -37,7 +37,7 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 launchRegisterIntent();
-                finish();
+                //finish();
             }
         });
 
